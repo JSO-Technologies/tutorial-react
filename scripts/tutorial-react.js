2e299e53536bb5fb9901742893c78cdd39c35f4c
JSO-Technologies <JSO.Technologies@gmail.com>
2014-11-22 13:49:14 +0100

    Component composition

--- scripts/tutorial-react.js
+++ scripts/tutorial-react.js
@@ -1,8 +1,30 @@
+var CommentList = React.createClass({
+	render: function() {
+		return (
+			<div className="commentList">
+			Here's come the comment list
+			</div>
+			);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function() {
+		return (
+			<div className="commentForm">
+			Here's come the comment form
+			</div>
+			);
+	}
+});
+
 var CommentBox = React.createClass({
 	render: function() {
 		return (
 			<div className="commentBox">
-				Here's come the comment box
+				<h1>Comments</h1>
+				<CommentList/>
+				<CommentForm/>
 			</div>
 		);
 	}
